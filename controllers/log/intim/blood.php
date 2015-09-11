e58ff5b5985ab8750efa04174995db22dddc0812
Martin <martin@mail.com>
2015-09-11 17:38:30 +0200

    Add checkout of intime callendar

--- controllers/log/intim/blood.php
+++ controllers/log/intim/blood.php
@@ -5,11 +5,35 @@
  * Date: 11.09.2015
  * Time: 15:45
  */
-$bloodContainer = "<option value='0'>Žádné</option>
-                    <option value='1'>Velmi slabé</option>
-                    <option value='2'>Slabé</option>
-                    <option value='3'>Střední</option>
-                    <option value='4'>Silné</option>
-                    <option value='5'>Velmi silné</option>";
+$bloodContainer = "<option value='0'";
+if($blood == 0){
+    $bloodContainer = " selected ";
+}
+$bloodContainer .= ">Žádné</option>
+                    <option value='1'";
+if($blood == 1){
+    $bloodContainer = " selected ";
+}
+$bloodContainer .= ">Velmi slabé</option>
+                    <option value='2'";
+if($blood == 2){
+    $bloodContainer = " selected ";
+}
+$bloodContainer .= ">Slabé</option>
+                    <option value='3'";
+if($blood == 3){
+    $bloodContainer = " selected ";
+}
+$bloodContainer .= ">Střední</option>
+                    <option value='4'";
+if($blood == 4){
+    $bloodContainer = " selected ";
+}
+$bloodContainer .= ">Silné</option>
+                    <option value='5'";
+if($blood == 5){
+    $bloodContainer = " selected ";
+}
+$bloodContainer .= ">Velmi silné</option>";
 
 return $bloodContainer;
--- controllers/log/intim_calendar.php
+++ controllers/log/intim_calendar.php
@@ -10,8 +10,45 @@ if($profil->getGender() != "f"){
 }
 $date = date("j. n. Y");
 $temperature = "36.0";
+$err = array();
+$blood = 0;
+
+if(isset($_POST['date'])){
+    /**
+     * DATUM
+     */
+    $time = $_POST['date'];
+    $time = str_replace(",", ".", $time);
+    $time = str_replace("-", ".", $time);
+    $time = explode(".", $time);
+    if(!isset($time[0]) || !isset($time[1]) || !isset($time[2])){
+        $err[] =  "Špatný formát datumu (DD. MM. RRRR)!";
+    }else{
+        $time[0] = trim ($time[0]);
+        $time[1] = trim ($time[1]);
+        $time[2] = trim ($time[2]);
+        $date = $time[0] . ". " . $time[1] . ". " .$time[2];
+    }
+
+    $temperature = $_POST['temperature'];
+    $temperature = str_replace(",", ".", $temperature);
+    $temperature = explode(".", $temperature);
+    if(!isset($temperature[1])){
+        $temperature[1] = "0";
+    }
+    if(strlen($temperature[0]) > 2){
+        $temperature[1] = substr($temperature[0], 2, 2);
+        $temperature[0] = substr($temperature[0], 0, 2);
+    }elseif(strlen($temperature[0]) < 2){
+       $err[] =  "Špatný formát teploty (TT.t)!";
+    }
+    $temperature = $temperature[0].'.'.$temperature[1];
+    $timestamp = strtotime($date);
+
+    $blood = $_POST['blood'];
+
+}
+
 $blood = include_once('controllers/log/intim/blood.php');
-
-
 $intimCalendarContainer = include_once('views/intim_calendar/calendar.php');
 return $intimCalendarContainer;
