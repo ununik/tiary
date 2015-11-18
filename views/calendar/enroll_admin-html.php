4d1ef862673210bc4f96e0a0ba0f432d202509cf
Martin Pribyl <martin.pribyl@lingoking.com>
2015-11-18 14:08:53 +0100

    last change update

--- views/calendar/enroll_admin-html.php
+++ views/calendar/enroll_admin-html.php
@@ -1,6 +1,6 @@
 <?php
 $container = "<h1>Nahlášení účastníci</h1>";
-$container .= "<b>Událost</b>: $mainTitle";
+$container .= "<b>Událost</b>: <u>$mainTitle</u>";
 $container .= $table;
 $container .= "<div id='underLine'>* V případě uvedení pohlaví: m - muž, f - žena</div>";
 return $container;
